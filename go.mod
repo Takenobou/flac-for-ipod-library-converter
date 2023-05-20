84ffb3e61e274ea82823eb9ffbcc86212dcbcd92
A A Martin <alexmartin153@live.co.uk>
2023-05-20 19:35:54 +0100

    add support for opus and add a bitrate option
    opus support includes copying and resizing cover artwork to work well on rockbox (320x320 pixels for speed and baseline for compatibility)

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module flac-to-aac-library-converter
 
 go 1.20
+
+require github.com/disintegration/imaging v1.6.2
+
+require golang.org/x/image v0.1.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/disintegration/imaging"
 	"io"
 	"os"
 	"os/exec"
@@ -14,17 +15,22 @@ import (
 var srcDir string
 var destDir string
 var numWorkers int
+var codec string
+var bitrate int
 
 type Job struct {
 	srcFile  string
 	destFile string
 	isFlac   bool
+	bitrate  int
 }
 
 func init() {
 	flag.StringVar(&srcDir, "src", "", "Source directory containing the music files.")
 	flag.StringVar(&destDir, "dest", "", "Destination directory where the converted files will be saved.")
 	flag.IntVar(&numWorkers, "workers", 5, "Number of workers for processing files.")
+	flag.StringVar(&codec, "codec", "aac", "Codec to use for conversion. Options are 'aac' and 'opus'.")
+	flag.IntVar(&bitrate, "bitrate", 192, "Bitrate for the output file.")
 }
 
 func copyFile(srcFile, destFile string) (err error) {
@@ -55,8 +61,16 @@ func copyFile(srcFile, destFile string) (err error) {
 	return nil
 }
 
-func convertFile(srcFile, destFile string) error {
-	cmd := exec.Command("qaac64.exe", "--cvbr", "192", "--ignorelength", "--copy-artwork", srcFile, "-o", destFile)
+func convertFile(srcFile, destFile string, codec string, bitrate int) error {
+	var cmd *exec.Cmd
+	if codec == "aac" {
+		cmd = exec.Command("qaac64.exe", "--cvbr", fmt.Sprintf("%d", bitrate), "--ignorelength", "--copy-artwork", srcFile, "-o", destFile)
+	} else if codec == "opus" {
+		cmd = exec.Command("opusenc", "--bitrate", fmt.Sprintf("%d", bitrate), srcFile, destFile)
+	} else {
+		return fmt.Errorf("unsupported codec: %s", codec)
+	}
+
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("conversion failed: %w", err)
 	}
@@ -64,7 +78,31 @@ func convertFile(srcFile, destFile string) error {
 	return nil
 }
 
-func worker(jobs <-chan Job, wg *sync.WaitGroup) {
+func resizeAndConvertImage(srcFile, destFile string) error {
+	if _, err := os.Stat(destFile); err == nil {
+		fmt.Printf("Skipping (file already exists): %s\n", destFile)
+		return nil
+	} else if !os.IsNotExist(err) {
+		return fmt.Errorf("error checking file: %w", err)
+	}
+
+	src, err := imaging.Open(srcFile)
+	if err != nil {
+		return fmt.Errorf("failed to open image: %w", err)
+	}
+
+	src = imaging.Resize(src, 320, 320, imaging.Lanczos)
+
+	err = imaging.Save(src, destFile, imaging.JPEGQuality(90))
+	if err != nil {
+		return fmt.Errorf("failed to save image: %w", err)
+	}
+
+	fmt.Printf("Cover saved successfully: %s\n", destFile)
+	return nil
+}
+
+func worker(jobs <-chan Job, wg *sync.WaitGroup, codec string) {
 	defer wg.Done()
 	for job := range jobs {
 		if _, err := os.Stat(job.destFile); err == nil {
@@ -77,7 +115,7 @@ func worker(jobs <-chan Job, wg *sync.WaitGroup) {
 
 		if job.isFlac {
 			fmt.Printf("Converting: %s\n", job.srcFile)
-			if err := convertFile(job.srcFile, job.destFile); err != nil {
+			if err := convertFile(job.srcFile, job.destFile, codec, job.bitrate); err != nil {
 				fmt.Printf("Error converting file: %s\n", err)
 			}
 		} else {
@@ -101,13 +139,16 @@ func main() {
 	fmt.Printf("Destination directory: %s\n", destDir)
 	fmt.Printf("Number of workers: %d\n", numWorkers)
 
+	if codec == "opus" {
+		bitrate = 160
+	}
+
 	jobs := make(chan Job)
 	var wg sync.WaitGroup
 
-	// Start the workers.
 	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
-		go worker(jobs, &wg)
+		go worker(jobs, &wg, codec)
 	}
 
 	err := filepath.Walk(srcDir, func(path string, f os.FileInfo, err error) error {
@@ -116,6 +157,20 @@ func main() {
 		}
 
 		if f.IsDir() {
+			if cover, err := os.Stat(filepath.Join(path, "cover.png")); err == nil && !cover.IsDir() {
+				relDir, err := filepath.Rel(srcDir, path)
+				if err != nil {
+					return fmt.Errorf("unable to determine relative directory path: %w", err)
+				}
+				destDir := filepath.Join(destDir, relDir)
+				if err := os.MkdirAll(destDir, 0755); err != nil {
+					return fmt.Errorf("unable to create destination directory: %w", err)
+				}
+				coverDestFile := filepath.Join(destDir, "cover.jpg")
+				if err := resizeAndConvertImage(filepath.Join(path, "cover.png"), coverDestFile); err != nil {
+					fmt.Printf("Error resizing and converting image: %s\n", err)
+				}
+			}
 			return nil
 		}
 
@@ -125,16 +180,15 @@ func main() {
 		}
 
 		destFile := filepath.Join(destDir, rel)
-		destDirPath := filepath.Dir(destFile)
-		if err := os.MkdirAll(destDirPath, 0755); err != nil {
-			return fmt.Errorf("unable to create destination directory: %w", err)
-		}
-
 		if strings.ToLower(filepath.Ext(path)) == ".flac" {
-			destFile = strings.TrimSuffix(destFile, filepath.Ext(destFile)) + ".m4a"
-			jobs <- Job{srcFile: path, destFile: destFile, isFlac: true}
+			if codec == "aac" {
+				destFile = strings.TrimSuffix(destFile, filepath.Ext(destFile)) + ".m4a"
+			} else if codec == "opus" {
+				destFile = strings.TrimSuffix(destFile, filepath.Ext(destFile)) + ".opus"
+			}
+			jobs <- Job{srcFile: path, destFile: destFile, isFlac: true, bitrate: bitrate}
 		} else if strings.ToLower(filepath.Ext(path)) == ".mp3" {
-			jobs <- Job{srcFile: path, destFile: destFile, isFlac: false}
+			jobs <- Job{srcFile: path, destFile: destFile, isFlac: false, bitrate: bitrate}
 		}
 
 		return nil
@@ -142,7 +196,6 @@ func main() {
 
 	close(jobs)
 
-	// Wait for all jobs to finish.
 	wg.Wait()
 
 	if err != nil {
